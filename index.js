be7c7610df339de9073acb19b4d5a61319de7a41
dagmawikk <dagmawikassa1@gmail.com>
2025-04-12 04:41:08 +0300

    implement database and integration

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
+app.use(express.static('dist'))
+
 const Person = require("./models/phonebook") 
 
 const morgan = require("morgan")
